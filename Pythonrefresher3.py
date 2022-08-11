c26fe81254d70772f21078a7194727a3bab28632
Julio Almada <julioc.almadaf@gmail.com>
2022-08-11 16:42:24 -0700

    mas comentariso

--- Pythonrefresher3.py
+++ Pythonrefresher3.py
@@ -1,4 +1,13 @@
 #return es como un break 
 #
 
+#lambda functions: para funciones simples
+#como una suma, pueden no tener un nombre, por que son usualmente una linea simple de codigo.
 
+add = lambda x,y: x+y
+
+print(add(5,7))
+
+#map function returna a map object of the results
+#after applyinh the given function to each item of an iterable object(list, etc.)
+#map(fun,iter)
